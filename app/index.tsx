d20519e0a2e91dda2de68f90c01caee3f1f7b18e
= <=>
2025-04-21 22:53:49 +0700

    init project

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeSCreen = () => {
+  return (
+    <View>
+      <Text>HomeSCreen</Text>
+    </View>
+  )
+}
+
+export default HomeSCreen
